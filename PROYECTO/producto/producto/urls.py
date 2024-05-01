b5277336e9bcaf5bea2989d45fefca99a6d4abdc
Matias <vargas99matias@gmail.com>
2024-05-01 15:18:01 -0300

    actualizacion 1

--- PROYECTO/producto/producto/urls.py
+++ PROYECTO/producto/producto/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for config project.
+URL configuration for producto project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,14 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
-
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("core.urls")),
-    path("cliente/",include("cliente.urls")),
-    path('productos/',include('producto.urls')),
-
 ]
